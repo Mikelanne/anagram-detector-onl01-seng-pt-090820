cade79ab94e7aee79cde5157a712c93913e5f1bb
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-10-03 16:47:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,18 @@
 # Your code goes here!
+
+require 'pry'
+
 class Anagram
-  attr_accessor :word 
+  attr_accessor :words 
   
-  def initialize(word)
-    @word = word 
+  def initialize(words)
+    @words = words 
   end 
   
+  def self.match(words)
+    binding.pry
+   words.split("") == words
   
+  end
+    
 end 
